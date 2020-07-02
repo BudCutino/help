82e39cd5198884a3b127d9c823bdd5b5c12c01d2
Bud Cutino <64506868+BudCutino@users.noreply.github.com>
2020-07-01 18:15:07 -0700

    testing string

--- index.py
+++ index.py
@@ -6,11 +6,11 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
 
         self.send_response(200)
-        self.send_header("Content-type", "application/json")
+        # self.send_header("Content-type", "application/json")
         self.end_headers()
 
-        response = {"hello": b()}
+        response = '{"hello": b()}'
     
-        self.wfile.write(json.dumps(response).encode("utf-8"))
+        self.wfile.write('json.dumps(response).encode("utf-8")')
 
         return
