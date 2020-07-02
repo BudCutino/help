9dc8518fc732b92eae13783a74dfa16a4ca5f4d5
Bud Cutino <64506868+BudCutino@users.noreply.github.com>
2020-07-01 18:30:45 -0700

    send plain text

--- index.py
+++ index.py
@@ -1,17 +1,18 @@
 from http.server import BaseHTTPRequestHandler
 import json
 from fn import b
+
 class handler(BaseHTTPRequestHandler):
+
+    message = "Hello World"
 
     def do_GET(self):
 
         self.send_response(200)
-        self.send_header("Content-type", "text/plain")
+        self.send_header("Content-type", "text/html; charset=UTF-8")
         # self.send_header("Content-type", "application/json")
         self.end_headers()
-
-        response = '{"hello": b()}'
     
-        self.wfile.write('json.dumps(response).encode("utf-8")')
+        self.wfile.write(self.message.encode("utf-8"))
 
         return
